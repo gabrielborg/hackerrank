be523f9b729b31d345cf51ea38676975fd6a33b7
glopes <gabrielborg@gmail.com>
2018-06-16 14:15:42 -0300

    Refactor code.

--- src/main/java/com/gabriellopes/crackingthecodinginterview/datastructures/StringsMakingAnagrams.java
+++ src/main/java/com/gabriellopes/crackingthecodinginterview/datastructures/StringsMakingAnagrams.java
@@ -17,7 +17,7 @@ public class StringsMakingAnagrams {
     final int ALPHABET_LETTERS_NUM = 26;
     final int FIRST_LETTER = 'a';
 
-    int lettersFreq[] = new int[ALPHABET_LETTERS_NUM];
+    int[] lettersFreq = new int[ALPHABET_LETTERS_NUM];
 
     for (int i = 0; i < a.length(); i++) {
       lettersFreq[a.charAt(i) - FIRST_LETTER]++;
