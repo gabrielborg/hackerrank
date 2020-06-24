0ab016a163c05c89b1eb6a1ad8458946f845afd1
Gabriel Borges <gabrielborg@gmail.com>
2020-06-24 15:57:15 +0200

    implements FizzBuzz.

---
+++ src/main/java/com/gabriellopes/leetcode/topinterviewquestions/math/FizzBuzz.java
@@ -0,0 +1,33 @@
+package com.gabriellopes.leetcode.topinterviewquestions.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if(i % 3 == 0) {
+                result.add("Fizz");
+            } else if(i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> result = fizzBuzz(15);
+
+        for(String s : result) {
+            System.out.println(s);
+        }
+    }
+}
